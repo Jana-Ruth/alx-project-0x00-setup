31045914513f5ed089f5ac44691f445ebf5f1a96
Jana-Ruth <ruthewenode73@gmail.com>
2025-07-17 06:31:11 +0100

    third commit

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -4,10 +4,17 @@ import Card from "@/components/Card"
 
 const Landing: React.FC = () => {
   return (
-    <div>
+    <div className="p-7">
         <h1 className="text-xl font-extralight align-middle">Landing Page</h1>
-        <Card/>
-        <Button title={"Buy"}/>
+          <div className="flex gap-4 flex-wrap">
+        <Card/> <Card/> <Card/><Card/>
+        </div>
+        <div className="flex gap-4 flex-wrap">
+        <Button title="Small Button" size="small" rounded="sm" />
+        <Button title="Medium Button" size="medium" rounded="md" />
+        <Button title="Large Button" size="large" rounded="full" />
+        <Button title="Custom Combo" size="large" rounded="sm" />
+      </div>
     </div>
   )
 }
